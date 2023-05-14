bf3aa9274eb678250d78b21cb96503bb695e2405
vallenov <a2lenov@mail.ru>
2023-05-14 15:09:16 +0300

    Add menu control from serial port

--- URC.ino
+++ URC.ino
@@ -15,20 +15,44 @@
 
 long timer = 0;
 
-LCD mlcd;
+LCD lcd;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("start");
-  mlcd.init();
+  lcd.init();
   
 }
 
 void loop() {
-  Serial.println("next");
-  if (millis() - ::timer > 2000) {
-    Serial.println("iter");
-    mlcd.act();
+  if (Serial.available() > 0) {
+    int s;
+    while (Serial.available() > 0) {
+      s = Serial.read();
+    }
+    Serial.print("Sim: ");
+    Serial.println(s);
+    if (s == 117) {
+      Serial.println("u");
+      lcd.menu_action(-1);
+    }
+    else if (s == 100) {
+      Serial.println("d");
+      lcd.menu_action(1);
+    }
+    else if (s == 62) {
+      Serial.println(">");
+      lcd.menu_action(0, 1);
+    }
+    else if (s == 60) {
+      Serial.println("<");
+      lcd.menu_action(0, -1);
+    }
+    lcd.battery++;
+    lcd.ping++;
+  }
+  if (millis() - ::timer > 500) {
+    //lcd.menu_action(-1);
+    lcd.show_frame();
     ::timer = millis();
   }
 }
