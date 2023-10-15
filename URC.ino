a0ad71579b4d1c1b8b1f0ff5b4d62f2b0baa6e40
vallenov <a2lenov@mail.ru>
2023-10-15 15:45:31 +0300

    little change

--- URC.ino
+++ URC.ino
@@ -149,52 +149,53 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
   myoled.init();
+  delay(3000);
   radioSetup();
 }
 
 void loop() {
 //  check_actual_rc_channel();
   transmit_pac();
-//  if (Serial.available() > 0) {
-//    int s;
-//    while (Serial.available() > 0) {
-//      s = Serial.read();
-//    }
-//    Serial.print("Sim: ");
-//    Serial.println(s);
-//    if (s == 117) {
-//      Serial.println("u");
-//      myoled.menu_action(-1);
-//    }
-//    else if (s == 100) {
-//      Serial.println("d");
-//      myoled.menu_action(1);
-//    }
-//    else if (s == 62) {
-//      Serial.println(">");
-//      myoled.menu_action(0, 1);
-//    }
-//    else if (s == 60) {
-//      Serial.println("<");
-//      myoled.menu_action(0, -1);
-//    }
-//    else if (s == 112) {
-//      Serial.println("p");
-//      myoled.menu_action(0, 0, true);
-//    }
-//    else if (s == 91) {
-//      Serial.println("[");
-//      myoled.menu_action(-1, 0, true);
-//    }
-//    else if (s == 93) {
-//      Serial.println("]");
-//      myoled.menu_action(1, 0, true);
-//    }
-//  }
+  if (Serial.available() > 0) {
+    int s;
+    while (Serial.available() > 0) {
+      s = Serial.read();
+    }
+    Serial.print("Sim: ");
+    Serial.println(s);
+    if (s == 117) {
+      Serial.println("u");
+      myoled.menu_action(-1);
+    }
+    else if (s == 100) {
+      Serial.println("d");
+      myoled.menu_action(1);
+    }
+    else if (s == 62) {
+      Serial.println(">");
+      myoled.menu_action(0, 1);
+    }
+    else if (s == 60) {
+      Serial.println("<");
+      myoled.menu_action(0, -1);
+    }
+    else if (s == 112) {
+      Serial.println("p");
+      myoled.menu_action(0, 0, true);
+    }
+    else if (s == 91) {
+      Serial.println("[");
+      myoled.menu_action(-1, 0, true);
+    }
+    else if (s == 93) {
+      Serial.println("]");
+      myoled.menu_action(1, 0, true);
+    }
+  }
   
-//  if (millis() - ::timer > 500) {
-//    //lcd.menu_action(-1);
-//    myoled.show_frame();
-//    ::timer = millis();
-//  }
+  if (millis() - ::timer > 500) {
+    //lcd.menu_action(-1);
+    myoled.show_frame();
+    ::timer = millis();
+  }
 }
